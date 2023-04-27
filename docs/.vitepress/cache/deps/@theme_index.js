4d6f77b6fe1eac78ae42112d0ee99a4f08039752
Ginyi <ginyi@aliyun.com>
2023-04-27 22:40:02 +0800

    随便写点儿~✔

--- docs/.vitepress/cache/deps/@theme_index.js
+++ docs/.vitepress/cache/deps/@theme_index.js
@@ -1,25 +1,25 @@
 // node_modules/vitepress/dist/client/theme-default/index.js
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/fonts.css";
 
 // node_modules/vitepress/dist/client/theme-default/without-fonts.js
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/base.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
-import "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
-import VPBadge from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
-import Layout from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/Layout.vue";
-import { default as default2 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
-import { default as default3 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
-import { default as default4 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
-import { default as default5 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
-import { default as default6 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
-import { default as default7 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
-import { default as default8 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
-import { default as default9 } from "H:/Data-Git/vue/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/vars.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/base.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/utils.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/custom-block.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-code-group.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-doc.css";
+import "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/styles/components/vp-sponsor.css";
+import VPBadge from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPBadge.vue";
+import Layout from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/Layout.vue";
+import { default as default2 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeHero.vue";
+import { default as default3 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeFeatures.vue";
+import { default as default4 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPHomeSponsors.vue";
+import { default as default5 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPDocAsideSponsors.vue";
+import { default as default6 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPage.vue";
+import { default as default7 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageTitle.vue";
+import { default as default8 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamPageSection.vue";
+import { default as default9 } from "E:/Data-Git/ginyi-code-notes/node_modules/vitepress/dist/client/theme-default/components/VPTeamMembers.vue";
 var theme = {
   Layout,
   enhanceApp: ({ app }) => {
--- docs/index.md
+++ docs/index.md
@@ -2,19 +2,19 @@
 layout: home
 
 hero:
-  name: 主标题
-  text: 内容信息
-  tagline: 副内容信息
+  name: Ginyi 学习笔记
+  text: 
+  tagline: 记录 Coding 时遇到的一些问题，总结归纳！
   image:
-    src: /logo.png
-    alt: 网站的 logo 图片
+    src: /images/logo.png
+    alt: logo
   actions:
     - theme: brand
       text: 快速开始
       link: /guide/what-is-vitepress
     - theme: alt
       text: 在 github 上查看
-      link: https://github.com/vuejs/vitepress
+      link: https://github.com/Ginyi3705
 features:
   - icon: ⚡️
     title: 这里是功能区 1
